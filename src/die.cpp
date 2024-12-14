be9704b76b9c349a29a5f50353139984aac9cab2
DanielCardenas94 <158984737+DanielCardenas94@users.noreply.github.com>
2024-12-14 03:52:05 +0000

    Question 1: Implemented Die class and test cases.

--- src/die.cpp
+++ src/die.cpp
@@ -1,1 +1,6 @@
-//
+#include "die.h"
+#include <cstdlib> // For rand()
+
+int Die::roll() {
+    return rand() % sides + 1; 
+}
--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -1,6 +1,12 @@
-#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+#include "../src/die.h"
 
-TEST_CASE("Verify Test Configuration", "verification") {
-	REQUIRE(true == true);
+TEST_CASE("Die rolls return values between 1 and 6") {
+    Die die;
+    for (int i = 0; i < 10; ++i) {
+        int roll_value = die.roll();
+        REQUIRE(roll_value >= 1);
+        REQUIRE(roll_value <= 6);
+    }
 }
