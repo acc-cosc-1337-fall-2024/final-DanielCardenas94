e76a068b8495897f14af7ef90600f7e2a4242ab5
DanielCardenas94 <158984737+DanielCardenas94@users.noreply.github.com>
2024-12-14 04:03:55 +0000

    Question 2: Implemented Roll class and test cases.

--- test/craps_test.cpp
+++ test/craps_test.cpp
@@ -10,3 +10,17 @@ TEST_CASE("Die rolls return values between 1 and 6") {
         REQUIRE(roll_value <= 6);
     }
 }
+
+#include "../src/roll.h"
+
+TEST_CASE("Roll combines two dice and returns values between 2 and 12") {
+    Die die1, die2;
+    Roll roll(die1, die2);
+    for (int i = 0; i < 10; ++i) {
+        roll.roll_dice();
+        int result = roll.roll_value();
+        REQUIRE(result >= 2);
+        REQUIRE(result <= 12);
+    }
+}
+
